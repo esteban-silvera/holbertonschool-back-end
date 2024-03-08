72251108c14d0cb62b86b3ef397af3c7581a753f
esteban-silvera <estebansilvera000@gmail.com>
2024-03-08 19:59:06 -0300

    2 and 3 ejercice

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """comentss"""
-import json
 from requests import get
 from sys import argv
+import json
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     response = get('https://jsonplaceholder.typicode.com/todos/')
     data = response.json()
 
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
+    ourdata = []
+    response2 = get('https://jsonplaceholder.typicode.com/users/')
     data2 = response2.json()
 
-    for i in data2:
-        if i['id'] == int(argv[1]):
-            u_name = i['username']
-            id_no = i['id']
+    for x in data2:
+        if x['id'] == int(argv[1]):
+            employee = x['username']
+            id_no = x['id']
 
-    row = []
+    ourdata = []
 
-    for i in data:
+    for x in data:
         new_dict = {}
-        if i['userId'] == int(argv[1]):
-            new_dict['username'] = u_name
-            new_dict['task'] = i['title']
-            new_dict['completed'] = i['completed']
-            row.append(new_dict)
+        if x['userId'] == int(argv[1]):
+            new_dict['username'] = employee
+            new_dict['task'] = x['title']
+            new_dict['completed'] = x['completed']
+            ourdata.append(new_dict)
 
     final_dict = {}
-    final_dict[id_no] = row
+    final_dict[id_no] = ourdata
     json_obj = json.dumps(final_dict)
 
-    with open(argv[1] + ".json",  "w") as f:
-        f.write(json_obj)
+    with open(argv[1] + '.json', 'w') as file:
+        file.write(json_obj)
