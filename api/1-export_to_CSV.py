0f12ede3a46de1baf2d580428698cdb48307f3ad
esteban-silvera <estebansilvera000@gmail.com>
2024-03-08 19:58:25 -0300

    1 ejercice

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,31 +2,30 @@
 """coments"""
 
 
-import csv
 from requests import get
 from sys import argv
+import csv
 
 
 if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
     data = response.json()
-
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
+    ourdata = []
+    response2 = get('https://jsonplaceholder.typicode.com/users/')
     data2 = response2.json()
 
-    for i in data2:
-        if i['id'] == int(argv[1]):
-            employee = i['username']
+    for x in data2:
+        if x['id'] == int(argv[1]):
+            employee = x['username']
 
-    with open(argv[1] + '.csv', 'w', newline='') as file:
-        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for i in data:
-            row = []
-            if i['userId'] == int(argv[1]):
-                row.append(i['userId'])
-                row.append(employee)
-                row.append(i['completed'])
-                row.append(i['title'])
+    with open(argv[1] + '.csv', 'w', newline='') as f:
+        writ = csv.writer(f, quoting=csv.QUOTE_ALL)
 
-                writ.writerow(row)
+        for x in data:
+            ourdata = []
+            if x['userId'] == int(argv[1]):
+                ourdata.append(x['userId'])
+                ourdata.append(employee)
+                ourdata.append(x['completed'])
+                ourdata.append(x['title'])
+                writ.writerow(ourdata)
