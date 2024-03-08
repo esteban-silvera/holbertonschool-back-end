093e423b86de02361d6085fbbaef56364e3a4946
esteban-silvera <estebansilvera000@gmail.com>
2024-03-08 19:27:58 -0300

    0 ejercice

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,43 +3,20 @@
 
 
 import requests
-import sys
+from sys import argv
 
-
-import requests
-
-
-def fetch_todo_list(employee_id):
-    """coment"""
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
-        employee_name = user_data["name"]
-
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
-        todo_data = todo_response.json()
-
-        completed_tasks = [task["title"] for task in todo_data if task["completed"]]
-        total_tasks = len(todo_data)
-        num_completed_tasks = len(completed_tasks)
-
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t{task}")
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-        sys.exit(1)
+    api_url = f"https://jsonplaceholder.typicode.com/"
 
-    employee_id = int(sys.argv[1])
-    fetch_todo_list(employee_id)
+    user_id = int(argv[1])
+    user_data = requests.get(api_url + f"users/{user_id}").json()
+    user_tasks = requests.get(api_url + f"users/{user_id}/todos").json()
+
+    user_completed_tasks = [t for t in user_tasks if t["completed"]]
+
+    print(f"Employee {user_data['name']} is done with ", end="")
+    print(f"tasks({len(user_completed_tasks)}/{len(user_tasks)}):")
+
+    for task in user_completed_tasks:
+        print("\t " + task["title"])
